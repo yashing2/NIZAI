489af384d0b6b25f7091bbdc4109a47c87e54a11
Nizzix <argggggg@hmu.rip>
2024-12-13 05:02:41 +0100

    Update v.v

--- v.v
+++ v.v
@@ -1,6 +1,8 @@
 {
   "version": "1.0",
   "discord": "https://discord.gg/vHYeafts",
+  "discord2": "http://discord.nizzix.ovh",
+  "download": "http://download.nizzix.ovh",
   "website": "http://nizzix.ovh",
   "free_key": "discord.nizzix.ovh"
 }
